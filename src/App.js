12cceb4cd9c8d7c175c8bb26c9a54f561d55d624
coelhothay <thay.csb@gmail.com>
2021-09-14 11:47:13 -0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle/>
+      <Switch>
+      <Route path="/" exact  />
+      <Route path="/sign-up" exact  />
+      <Route path="/timeline" exact  />
+      <Route path="/my-posts" exact />
+      <Route path="/user/:id" exact  />
+      <Route path="/hashtagh/:hashtag" exact/>
+      <Route path="/my-likes" exact  />
+      </Switch>
+    </Router>
   );
 }
 
