00af6c0ca6e5c4c34ddb9d366e2ad7f7427978dc
victordurco <victordurco@gmail.com>
2021-09-15 00:06:28 -0300

    CreatePost layout done

--- src/pages/Timeline/CreatePost.jsx
+++ src/pages/Timeline/CreatePost.jsx
@@ -4,7 +4,17 @@ import React from 'react';
 export default function CreatePost() {
 	return (
 		<Container>
-			oi
+			<img src='https://pbs.twimg.com/media/ECa2_i3W4AEm5jd.jpg' alt='perfil' />
+			<PostContent>
+				<form>
+					<h2>O que você tem pra favoritar hoje?</h2>
+					<fieldset>
+						<Link type='url' placeholder='http:// ...' />
+						<LinkDescription  placeholder='Comente alguma coisa sobre esse link' />
+						<button type='submit'>Publicar</button>
+					</fieldset>
+				</form>
+			</PostContent>
 		</Container>
 	);
 }
@@ -15,5 +25,74 @@ const Container = styled.div`
 	border-radius: 16px;
 	background-color: #ffffff;
 	box-shadow: 0px 4px 4px 0px #00000040;
+	padding: 16px 22px 16px 18px;
+	display: flex;
+	flex-direction: initial;
+	position: relative;
+	top: 0;
+	left: 0;
+	margin: auto auto;
+	font-family: 'Lato', sans-serif;
+	font-weight: 300;
+
+	img{
+		height: 50px;
+		width: 50px;
+		border-radius: 26.5px;
+	}
 `;
 
+const PostContent = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-left: 18px;
+
+	h2{
+		color: #707070;
+		font-size: 20px;
+		margin-bottom: 17px;
+	}
+
+	button{
+		position: absolute;
+		right: 22px;
+		bottom: 16px;
+		width: 112px;
+		height: 31px;
+		background-color: #1877F2;
+		color: #FFF;
+		border-radius: 5px;
+		font-size: 17px;
+		font-family: 'Lato', sans-serif;
+		font-weight: 700;
+	}
+`;
+
+const Link = styled.input`
+		width: 503px;
+		height: 30px;
+		border-radius: 5px;
+		background-color: #EFEFEF;
+		border: none;
+		margin-bottom:5px;
+		padding: 5px 13px;
+		font-size: 15px;
+		font-family: 'Lato', sans-serif;
+		font-weight: 300;
+		color: #4a4a4a;
+`;
+
+const LinkDescription = styled.textarea`
+		width: 503px;
+		height: 66px;
+		border-radius: 5px;
+		background-color: #EFEFEF;
+		border: none;
+		margin-bottom:5px;
+		padding: 8px 12px;
+		font-size: 15px;
+		font-family: 'Lato', sans-serif;
+		font-weight: 300;
+		color: #4a4a4a;
+
+`;
