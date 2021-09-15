1e9a47f85ae45f6b0c16d9df6cb8a906d369a211
victordurco <victordurco@gmail.com>
2021-09-14 23:13:53 -0300

    CreatePost first commit

---
+++ src/pages/Timeline/CreatePost.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import React from 'react';
+
+export default function CreatePost() {
+	return (
+		<Container>
+			oi
+		</Container>
+	);
+}
+
+const Container = styled.div`
+	width: 611px;
+	height: 209px;
+	border-radius: 16px;
+	background-color: #ffffff;
+	box-shadow: 0px 4px 4px 0px #00000040;
+`;
+
