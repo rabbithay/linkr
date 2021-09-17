797693984784de82e24436ff32a25f2b488b0042
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-09-17 18:49:10 -0300

    Initialize my-likes page

--- src/pages/MyLikes/index.jsx
+++ src/pages/MyLikes/index.jsx
@@ -1,6 +1,114 @@
-import React from 'react';
-export default function MyLikes(){
+import React, { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import UserContext from '../../contexts/UserContext';
+import { getMyLikedPosts } from '../../service/service.posts';
+
+import Post from '../shared/Post';
+import pageReloadErrorAlert from '../shared/pageReloadErrorAlert';
+import Header from '../shared/Header';
+import CirclesLoader from '../shared/CirclesLoader';
+import Trending from '../shared/Trending';
+import NoPostMessage from '../shared/NoPostMessage';
+
+
+export default function Hashtag(){
+	const [likedPostsList, setLikedPostsList] = useState([]);
+	const [loaderIsActive, setLoaderIsActive] = useState(false);
+	const { userInfo } = useContext(UserContext);
+
+	function loadHashtagPosts(){		
+		setLoaderIsActive(true);
+		getMyLikedPosts(userInfo.token).then(({ data: { posts } })=>{
+			console.log('posts:', posts);
+			setLikedPostsList(posts);
+		}).catch(()=>{
+			pageReloadErrorAlert();
+		}).finally(()=>{
+			setLoaderIsActive(false);
+		});
+	}
+	
+	useEffect(()=>{
+		// Tem um delay para atualizar o token com o LocalStorage,
+		// por isso fica verificando quando o token não é undefined
+		if (userInfo.token) loadHashtagPosts();
+	},[userInfo.token]);
+
 	return(
-		<></>
+		<>	
+			<Header/>
+			<Background>
+				<TimelineContent>
+					<h1># my likes</h1>
+					{/* TODO: Seria bacana colocar o nome dessa função para somente um loadPosts */}
+					{loaderIsActive 
+						? <CirclesLoader/>
+						: (likedPostsList.length)
+							?  likedPostsList.map((p)=>{
+								return (
+									<Post key={p.id} postInfo={p} />
+								);
+							})
+							: <NoPostMessage/>
+					}
+				</TimelineContent>
+				<HashtagContainer>
+					<Trending/>
+				</HashtagContainer>
+			</Background>
+		</>
 	);
 }
+
+const Background = styled.div`
+	width: 100%;
+	min-width: 100vw;
+	height: auto;
+	min-height: 100vh;
+	display: inline-flex;
+	background-color: #333;
+	justify-content: center;
+	gap: 25px;
+	padding: 72px;
+	@media (max-width: 611px) {
+		padding: 19px 0px;
+    }
+`;
+const TimelineContent = styled.div`
+	width: 611px;
+	height: auto;
+	h1 {
+		font-family: 'Oswald';
+		font-weight: 700;
+		font-size: 43px;
+		color: #fff;
+		margin: 53px 0px 43px 0px;
+		justify-content: flex-start;
+		line-height: 63px;
+		word-break: break-all;
+		@media (max-width: 611px) {
+			margin: 53px 0px 12px 17px;
+			font-size: 33px;
+			line-height: 49px;
+		}
+	}
+	@media (max-width: 611px) {
+		width: 100vw;    
+	}
+`;
+
+
+const HashtagContainer = styled.div`
+	width: 301px; 
+	height: 406px;
+	background-color: #333;
+	margin-top: 160px;
+	border-radius: 16px;
+	@media (max-width: 1024px) {
+		display: none;
+	}
+	position: sticky;
+	top: 80px;
+`;
+
--- src/service/service.posts.js
+++ src/service/service.posts.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 const BASE_URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/linkr';
 
+
+const makeConfig = (token) => {
+	const config = {
+		headers: {
+			'Authorization': `Bearer ${token}`
+		}
+	};
+	
+	return config;
+};
+
 function getPosts (config) {
 	return axios.get(`${BASE_URL}/posts`, config);
 }
@@ -14,22 +25,16 @@ const createPostAPI = (text, link, token) => {
 		text,
 		link
 	};
-	const config = {
-		headers: {
-			'Authorization': `Bearer ${token}`
-		}
-	};
-	const promise = axios.post(`${BASE_URL}/posts`, body, config);
-	return promise;
+	return axios.post(`${BASE_URL}/posts`, body, makeConfig(token));
 };
 
 const getSomeonesPosts = (userId, token) => {
-	const config = {
-		headers: {
-			'Authorization': `Bearer ${token}`
-		}
-	};
-	const promise = axios.get(`${BASE_URL}/users/${userId}/posts`, config);
+	return axios.get(`${BASE_URL}/users/${userId}/posts`, makeConfig(token));
+};
+
+const getMyLikedPosts = (token) => {
+	const promise = axios.get(`${BASE_URL}/posts/liked`, makeConfig(token));
+	console.log('posts que curti: ', promise);
 	return promise;
 };
 
@@ -38,5 +43,6 @@ export {
 	getPosts,
 	createPostAPI,
 	getUserPosts,
-	getSomeonesPosts
+	getSomeonesPosts,
+	getMyLikedPosts
 };
